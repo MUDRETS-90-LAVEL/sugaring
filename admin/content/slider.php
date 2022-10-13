84be29bc2edeee63c63ef6390c6ccf026212f954
Smykow Sergei <s.smikow@outlook.com>
2022-10-13 14:44:12 +0300

    slider_v1

--- admin/content/slider.php
+++ admin/content/slider.php
@@ -8,23 +8,25 @@
         <div class="content_body">
             <div class="info">
                 <h2 class="form_title"><?=$title;?></h2>
-
-                
-                <!-- Сдесь выводить циклом надо  -->
-
-                <?php foreach($arr as $elem): ?>
-                        <div class="item_wrapp">
-                            <div class="item_img"><img class="img_elem" src="/img/slider/<?=$elem['img']?>" alt="<?=$elem['title']?>"></div>
-                            <div class="item_info">
-                                <p><label>Название</label>
-                                <input type="text" value="<?=$elem['title']?>" disabled></p>
-                                <p><label>Краткая информация</label>
-                                <textarea disabled><?=$elem['mini_description']?></textarea></p>
-                                <p><label>Ссылка</label>
-                                <input type="url" value="<?=$elem['link']?>" disabled></p>
+                <div class="btn_add"><a href="" class="add_elem">Добавить слайдер</a></div>
+                <div class="item_content">
+                    <?php foreach($arr as $elem): ?>
+                        <div class="elem">
+                            <div class="elem_img">
+                                <img class="img_elem" src="/img/slider/<?=$elem['img']?>" alt="<?=$elem['title']?>">
+                            </div>
+                            <div class="elem_title">
+                                <p class="slideer_title"><?=$elem['title']?></p>
+                            </div>
+                            <div class="btm_item">
+                                <div class="elem_btm">
+                                    <a href="" class="btl del" name = "<?=$elem['id']?>">Удалить</a>
+                                    <a href="" class="btl edit" name = "<?=$elem['id']?>">Редактировать</a>
+                                </div>
                             </div>
                         </div>
-                <?php endforeach;?>       
+                    <?php endforeach;?>
+                </div>
             </div>
         </div>
     </div>
